6501fe09258ef63cd77546655718f796a04b8f97
BMeus <bartosz.meus@live.com>
2015-12-14 23:25:41 +0100

    performance improved (splines), SD calculated properly

--- python/EMD.py
+++ python/EMD.py
@@ -6,27 +6,32 @@ from Hilbert import Hilbert
 
 class EMD(object):
 
-    SD_FACTOR = 0.3
+    SD_LIMIT_VAL = 0.3
 
-    def __init__(self, ecgSignal, samplingFrequency=360.0, imfsNo=1):
+    def __init__(self, ecgSignal, samplingFrequency=360.0, tWindowSize=120.0, imfsNo=1):
         self.ecgSignal = array(ecgSignal)
         self.ecgSignalSize = len(ecgSignal)
         self.samplingFrequency = samplingFrequency
+        self.tWindowSize = tWindowSize
+        self.nWindowSize = int(tWindowSize * samplingFrequency)
         self.rPeaks = []
         self.imfsNo = imfsNo
 
     def process(self):
         self.imfs = self.findIMFs(self.imfsNo)
-        self.hilbertAlgorithm = Hilbert(np.sum(self.imfs, axis=0), self.samplingFrequency)
-        self.rPeaks = self.hilbertAlgorithm.process()
+        self.hilbertAlgorithm = Hilbert(self.ecgSignal, self.tWindowSize, self.samplingFrequency)
+        self.rPeaks = self.hilbertAlgorithm.process(np.sum(self.imfs, axis=0))
         return self.rPeaks
 
-    def findIMFs(self, n=3):
-        part_size = 1000
+    def findIMFs(self, n=1):
+        part_size = 100000
         max_parts = int(math.ceil(float(self.ecgSignalSize) / float(part_size)))
         imfs = [[] for x in xrange(n)]
 
         for p in range(0, max_parts * part_size, part_size):
+            print p
+
+            residue = False
 
             N = self.ecgSignalSize - p if p == (max_parts - 1) * part_size else part_size
 
@@ -37,24 +42,33 @@ class EMD(object):
 
             for i in range(0, n):
                 h = np.copy(c)
-                sd = 1
-                maxmin_length = 0
+                sd = np.inf
 
-                prev_h = np.empty(N)
+                prev_h = np.copy(h)
 
-                while sd > EMD.SD_FACTOR:
-                    maxima, minima = self.findExtrema(x_part, addBoundaries=True)
+                while sd > EMD.SD_LIMIT_VAL:
+                    maxima, minima = self.findExtrema(h)
 
                     # is it already a residue?
-                    maxmin_length = len(maxima) + len(minima)
-                    if maxmin_length < 2:
+                    if len(maxima) + len(minima) < 2:
+                        residue = True
                         break
 
-                    maxima_val = [x_part[j] for j in maxima]
-                    minima_val = [x_part[j] for j in minima]
+                    maxima = np.insert(maxima, 0, 0)
+                    minima = np.insert(minima, 0, 0)
+                    maxima = np.append(maxima, len(h) - 1)
+                    minima = np.append(minima, len(h) - 1)
 
-                    upper_bound_func = spi.interp1d(maxima, maxima_val, kind='cubic', copy=True)
-                    lower_bound_func = spi.interp1d(minima, minima_val, kind='cubic', copy=True)
+                    maxima_val = [h[j] for j in maxima]
+                    minima_val = [h[j] for j in minima]
+
+                    upper_bound_func = spi.InterpolatedUnivariateSpline(maxima,
+                                                                        maxima_val,
+                                                                        k=3)
+                    lower_bound_func = spi.InterpolatedUnivariateSpline(minima,
+                                                                        minima_val,
+                                                                        k=3)
+
 
                     x_new = np.arange(0, N, 1)
                     upper_bound = upper_bound_func(x_new)
@@ -62,14 +76,14 @@ class EMD(object):
 
                     m = np.add(lower_bound, upper_bound) * 0.5
 
-                    prev_h = np.copy(h)
-                    h = x_part - m
+                    h = prev_h - m
 
                     sd = self.calculateSD(prev_h, h, eps=0.0000001)
+                    prev_h = np.copy(h)
 
                 imfs_parts[i] = h
 
-                if maxmin_length < 2:
+                if residue:
                     break
 
                 c = np.subtract(c, h)
@@ -81,13 +95,11 @@ class EMD(object):
     def calculateSD(self, previous, current, eps=0.0):
         eps_array = np.empty(len(current))
         eps_array.fill(eps)
-        # (previous-current)^2/(current^2+eps)
-        numerator = np.square(np.subtract(previous, current))
-        denominator = np.add(np.square(current), eps_array)
-        summands = np.divide(numerator, denominator)
-        return np.sum(summands)
+        numerator = np.sum(np.square(np.subtract(previous, current)))
+        denominator = np.sum(np.add(np.square(previous), eps_array))
+        return np.divide(numerator, denominator)
 
-    def findExtrema(self, signal, addBoundaries=True):
+    def findExtrema(self, signal):
 
         diffSignal = np.diff(signal)
 
@@ -105,12 +117,4 @@ class EMD(object):
             elif np.sign(diffSignal[i]) == -1 and np.sign(diffSignal[i+1]) == 1:
                 minima = np.append(minima, i + 1)
 
-        if addBoundaries:
-            maxima = np.insert(maxima, 0, 0)
-            minima = np.insert(minima, 0, 0)
-            maxima = np.append(maxima, len(signal) - 1)
-            minima = np.append(minima, len(signal) - 1)
-
         return maxima, minima
-
-
